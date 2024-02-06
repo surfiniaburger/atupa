debd0422f0e6c44b29ed5843181d3a734373cc45
surfiniaburger <jdmasciano2@gmail.com>
2024-02-06 10:46:02 +0100

    removed schema

--- src/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS query_results (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  query TEXT NOT NULL,
-  result TEXT NOT NULL
-);
-CREATE INDEX idx_query_results_query ON query_results (query);
-
-INSERT INTO query_results (query, result) VALUES ('example query', 'example result');
